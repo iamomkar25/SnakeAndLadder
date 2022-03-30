e32e4d39d5afe3a7ba4aae6cef3d36dc443c34b4
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-03-30 15:25:57 +0530

    playerposition

---
+++ RollDice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder
+{
+    internal class RollDice
+    {
+        public const int Start = 0;
+        public const int End = 100;
+        static void Main(string[] args)
+        {
+            int currentPosition = 0;
+            Console.WriteLine("Welcome to Snake and Ladder Problem!");
+            Console.WriteLine("Starting position is : " + currentPosition);
+            //Create Reference for Random class
+            Random random = new Random();
+            int numberOnDie = random.Next(1, 7);
+            Console.WriteLine("Number on the die rolled is : " + numberOnDie);
+        }
+    }
+}
