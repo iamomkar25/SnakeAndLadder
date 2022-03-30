4cbde34c871039d60a617d1a0cc968514df07905
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-03-30 16:43:16 +0530

    Added-repeat

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_Ladder_Game_repeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Welcome to snake Ladder Game");
+
+                int position = 0;
+                while (position < 100)
+                {
+                    Random random = new Random();
+                    int Diceroll = random.Next(1, 7);
+                    if (position < 0)
+                    {
+                        position = 0;
+                    }
+
+                    Console.WriteLine("Diceroll : " + Diceroll);
+
+
+
+                    Random randomcheck = new Random();
+                    int Checkoption = randomcheck.Next(0, 3);
+
+                    switch (Checkoption)
+                    {
+                        case 0:
+                            position = position;
+                            Console.WriteLine("player statyed at position " + position);
+
+                            break;
+                        case 1:
+                            position = position + Diceroll;
+                            Console.WriteLine("player moved to position " + position);
+                            break;
+                        case 2:
+                            position = position - Diceroll;
+                            Console.WriteLine("player back to position " + position);
+                            break;
+
+                    }
+
+
+
+                }
+
+            }
+        }
+    }
+
